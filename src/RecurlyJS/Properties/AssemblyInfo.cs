dfbabc0a4952bd694527e1bfef6ff1ea03404dcc
Martin Høst Normark <m@martinnormark.com>
2012-11-01 21:04:50 +0100

    Add Nu Get package
    Add basic assembly info

--- src/RecurlyJS/Properties/AssemblyInfo.cs
+++ src/RecurlyJS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RecurlyJS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Un-official .Net library for using Recurly.js")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Milkshake Software")]
 [assembly: AssemblyProduct("RecurlyJS")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
